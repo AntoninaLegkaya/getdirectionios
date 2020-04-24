e04ad63c4edea92ded55253306969c9055301d26
Antonina <a.lehka@mobidev.biz>
2020-04-24 13:05:34 +0300

    Initial commit

---
+++ GetDirection/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GetDirection
+//
+//  Created by User on 24.04.2020.
+//  Copyright © 2020 User. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemBlue
+    }
+
+
+}
+
